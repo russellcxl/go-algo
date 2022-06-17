19e4b671495419cc4dd71605aa2abea7adc11664
Russell Chan <russell.chanxl@shopee.com>
2022-06-17 20:37:47 +0800

    forgot to add load balancer in previous commit

--- load_balancer/main.go
+++ load_balancer/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+// ----------------------------- server --------------------------------
+
+type Server interface {
+	// Address returns the address with which to access the server
+	Address() string
+
+	// IsAlive returns true if the server is alive and able to serve requests
+	IsAlive() bool
+
+	// Serve uses this server to process the request
+	Serve(rw http.ResponseWriter, req *http.Request)
+}
+
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+func (s *simpleServer) Address() string { return s.addr }
+
+func (s *simpleServer) IsAlive() bool { return true }
+
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
+}
+
+// ----------------------------- load balancer --------------------------------
+
 type LoadBalancer struct {
 	port            string
 	roundRobinCount int
@@ -14,7 +62,47 @@ func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
 	}
 }
 
-func main() {
+// getNextServerAddr returns the address of the next available server to send a
+// request to, using a simple round-robin algorithm
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
 
+	return server
+}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, req *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+
+	// log stuff about the request here
+	fmt.Printf("forwarding request to address %q\n", targetServer.Address())
+
+	// delete pre-existing X-Forwarded-For header to prevent IP spoofing
+	targetServer.Serve(rw, req)
+}
+
+// ----------------------------- main --------------------------------
+
+func main() {
+	servers := []Server{
+		newSimpleServer("https://www.google.com"),
+		newSimpleServer("https://www.bing.com"),
+		newSimpleServer("https://www.duckduckgo.com"),
+	}
+
+	lb := NewLoadBalancer("8000", servers)
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serveProxy(rw, req)
+	}
+
+	// register a proxy handler to handle all requests
+	http.HandleFunc("/", handleRedirect)
+
+	fmt.Printf("serving requests at 'localhost:%s'\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
 }
 
